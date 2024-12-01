21895206dc780caa9b711738a4fea19898108ddf
Rana <amrana@deloitte.com>
2024-12-01 17:07:58 +0530

    Initial commit for config server

---
+++ src/main/java/com/mbrdi/MbrdiMyworkConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mbrdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbrdiMyworkConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbrdiMyworkConfigServerApplication.class, args);
+	}
+
+}
